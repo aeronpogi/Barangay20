841fad60b141479849eb9de97cdf1c612ca54953
Aeron <aerskargu21@gmail.com>
2024-01-05 09:09:10 +0800

    first commit

--- admin/include/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "root", "", "barangay206")or die(mysqli_connect_error());
-
-?>
--- admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("include/session.php");
-
-unset($_SESSION["user_id"]);
-header("location: ../../index.php");
-?>
